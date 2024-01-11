5ac91e0ce916882506e346c52a5d904d4e9e6693
saragrosser <sgrosser00@gmail.com>
2024-01-11 15:11:55 +0100

    Add pokemonList array with three Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-alert('Hello world');
+let pokemonList = [
+{
+    name: 'pikachu',
+    height: '3',
+    type: 'electric',
+},
 
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+{
+    name:'Squirtle',
+    height: '6',
+    type: 'water',
 
+},
+
+{
+    name: 'Bulbasaur',
+    height: '5',
+    type: ['poison', 'grass'],
+   
+}];
+
+console.log(pokemonList); 
 
