daf08b9c14a5b506fff68b715e39342cbeb5d3df
saragrosser <sgrosser00@gmail.com>
2024-01-18 17:04:34 +0100

    IIFE added to pokemonList + adjusted forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,42 @@
-let pokemonList = [
-    {
-        name: 'Pikachu',
-        height: 3, // The height should be a number, not a string
-        type: ['electric'], 
-    },
-    {
-        name: 'Squirtle',
-        height: 6, // 
-        type: ['water'], 
-    },
-    {
-        name: 'Bulbasaur',
-        height: 5, // 
-        type: ['poison', 'grass'], // Correctly used an array of strings
-    }];
-    
-    console.log(pokemonList); 
-    
-    // Print the list of names from pokemonList and corresponding height of that name
-    pokemonList.forEach (function (pokemon){
-        // Print a message if the height is bigger than 5
-        if (pokemon.height > 5) {
-            document.write('<p>' + pokemon.name + ' - ' + pokemon.height + 'Wow, that\'s big!</p>');
-        } else {
-            document.write('<p>' + pokemon.name + ' - ' + pokemon.height + ';</p>');
-        }
-    });
+
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Pikachu',
+            height: 3, // The height should be a number, not a string don't use ''
+            type: ['electric'],
+        },
+        {
+            name: 'Squirtle',
+            height: 6, // 
+            type: ['water'],
+        },
+        {
+            name: 'Bulbasaur',
+            height: 5, // 
+            type: ['poison', 'grass'], // Correctly used an array of strings
+        }];
+    function getAll() {
+        return pokemonList;
+    }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+console.log(pokemonRepository);
+
+// Print the list of names from pokemonList and corresponding height of that name
+pokemonRepository.getAll().forEach(function (pokemon) {
+    // Print a message if the height is bigger than 5
+    if (pokemon.height > 5) {
+        document.write('<p>' + pokemon.name + ' - ' + pokemon.height + ';' + 'Wow, that\'s big!</p>');
+    } else {
+        document.write('<p>' + pokemon.name + ' - ' + pokemon.height + ';</p>');
+    }
+});
